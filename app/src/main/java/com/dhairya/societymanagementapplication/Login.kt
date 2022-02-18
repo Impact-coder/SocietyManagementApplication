16b70f6bf690ebeafa33f44666c198e5020ce074
Hardik <hardikvaghasiya69@gmail.com>
2022-02-18 23:21:23 +0530

    commit 2

--- app/src/main/java/com/dhairya/societymanagementapplication/Login.kt
+++ app/src/main/java/com/dhairya/societymanagementapplication/Login.kt
@@ -2,10 +2,16 @@ package com.dhairya.societymanagementapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 
 class Login : AppCompatActivity() {
+
+    private val auth = FirebaseAuth.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
     }
 }
