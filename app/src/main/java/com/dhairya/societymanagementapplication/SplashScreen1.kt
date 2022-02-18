61ed4d5ff29f072b4c833b0f4cefb56e029b02b3
Hardik <hardikvaghasiya69@gmail.com>
2022-02-19 01:36:12 +0530

    commit 6

---
+++ app/src/main/java/com/dhairya/societymanagementapplication/SplashScreen1.kt
@@ -0,0 +1,57 @@
+package com.dhairya.societymanagementapplication
+
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import androidx.appcompat.widget.AppCompatButton
+
+class SplashScreen1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen1)
+        setStatusBarTransparent()
+        supportActionBar?.hide()
+
+        var intro1_next_btn = findViewById<AppCompatButton>(R.id.intro1_next_btn)
+
+        intro1_next_btn.setOnClickListener {
+            Intent(this,SplashScreen2::class.java).apply {
+                startActivity(this)
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+            }
+        }
+
+
+
+    }
+
+    private fun setStatusBarTransparent() {
+        if (Build.VERSION.SDK_INT in 19..20){
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
+                setWindowFlag(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, true)
+            }
+        }
+        if (Build.VERSION.SDK_INT >= 19) {
+            window.decorView.systemUiVisibility= View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        }
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            setWindowFlag(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false)
+            window.statusBarColor = Color.TRANSPARENT
+        }
+    }
+
+    private fun setWindowFlag(bits: Int, on: Boolean){
+        val winParameters=window.attributes
+        if(on) {
+            winParameters.flags = winParameters.flags or bits
+        }else{
+            winParameters.flags=winParameters.flags and bits.inv()
+        }
+        window.attributes=winParameters
+    }
+}
