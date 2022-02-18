8f3c203eb99dfe77709cc38c05f9b6d0129a9671
Hardik <hardikvaghasiya69@gmail.com>
2022-02-18 23:03:27 +0530

    commit 2

--- app/src/main/java/com/dhairya/societymanagementapplication/Login.kt
+++ app/src/main/java/com/dhairya/societymanagementapplication/Login.kt
@@ -3,9 +3,9 @@ package com.dhairya.societymanagementapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
