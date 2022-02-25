3d71fbb64b05dfbda3125f91febce98f1a26bf8f
Hardik <hardikvaghasiya69@gmail.com>
2022-02-25 19:36:37 +0530

    commit 7

--- app/src/main/java/com/dhairya/societymanagementapplication/changePassword.kt
+++ app/src/main/java/com/dhairya/societymanagementapplication/changePassword.kt
@@ -2,10 +2,17 @@ package com.dhairya.societymanagementapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.textfield.TextInputEditText
 
 class changePassword : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_change_password)
+
+        val oldPassword = findViewById<TextInputEditText>(R.id.old_pass_edittext)
+        val newPassword = findViewById<TextInputEditText>(R.id.new_pass_edittext)
+        val confirmPassword = findViewById<TextInputEditText>(R.id.confirm_pass_edittext)
+
+
     }
 }
