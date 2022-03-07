05099b1112854e5db0a322bd2eee8a54fdfded93
Hardik <hardikvaghasiya69@gmail.com>
2022-03-08 01:35:17 +0530

    commit 7.9

--- app/src/main/java/com/dhairya/societymanagementapplication/data/residentsData.kt
+++ app/src/main/java/com/dhairya/societymanagementapplication/data/residentsData.kt
@@ -1,9 +1,12 @@
 package com.dhairya.societymanagementapplication.data
 
+import com.dhairya.societymanagementapplication.changePassword
+
 data class residentsData (
     val uid: String = "",
     val flatNo: String = "",
     val email: String = "",
-    val role:String = ""
+    val role:String = "",
+    val password: String =""
 
 )
