71ca9b028a60bb72ddfa1800f88425d211aa76ca
sdhairya <sdhairya412@gmail.com>
2022-03-08 00:45:23 +0530

    cp

--- app/src/main/java/com/dhairya/societymanagementapplication/dashboardActivity/addMember/addMemberFragment.kt
+++ app/src/main/java/com/dhairya/societymanagementapplication/dashboardActivity/addMember/addMemberFragment.kt
@@ -2,17 +2,22 @@ package com.dhairya.societymanagementapplication.dashboardActivity.addMember
 
 import android.os.Bundle
 import android.view.View
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.dhairya.societymanagementapplication.R
 import com.dhairya.societymanagementapplication.databinding.FragmentAddMemberBinding
+import com.google.android.material.snackbar.Snackbar
 
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-class addMemberFragment : Fragment() {
+class addMemberFragment : Fragment(R.layout.fragment_add_member) {
     private val viewModel: addMemberViewModel by viewModels()
     private lateinit var binding: FragmentAddMemberBinding
 
@@ -40,6 +45,15 @@ class addMemberFragment : Fragment() {
                 viewModel.addMember()
 
             }
+
+
+            radioGroupMemberRoles.setOnCheckedChangeListener { radioGroup, i ->
+                val radio= if (radioButton1.id == i) "member" else "Treasurer"
+
+                Snackbar.make(requireView(),radio , Snackbar.LENGTH_LONG).show()
+            }
+
+
 
             btnDone.setOnClickListener {
                 findNavController().navigate(addMemberFragmentDirections.actionAddMemberFragmentToDashBoardFragment())
