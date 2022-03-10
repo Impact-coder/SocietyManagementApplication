86cea44c1e954d0719c70daf0b5a74b9560a9ec7
Hardik <hardikvaghasiya69@gmail.com>
2022-03-10 21:01:31 +0530

    commit 7.9

--- app/src/main/java/com/dhairya/societymanagementapplication/authActivity/authfragments/ui/createProfile/createProfileFragment.kt
+++ app/src/main/java/com/dhairya/societymanagementapplication/authActivity/authfragments/ui/createProfile/createProfileFragment.kt
@@ -1,60 +1,78 @@
 package com.dhairya.societymanagementapplication.authActivity.authfragments.ui.createProfile
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import androidx.navigation.fragment.findNavController
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.dhairya.societymanagementapplication.R
+import com.dhairya.societymanagementapplication.dashboardActivity.DashboardActivity
+import com.dhairya.societymanagementapplication.databinding.FragmentLoginBinding
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.flow.collect
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [createProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class createProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class createProfileFragment : Fragment(R.layout.fragment_create_profile) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private val viewModel: createProfileViewModel by viewModel()
+    private lateinit var binding: FragmentLoginBinding
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_profile, container, false)
-    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment createProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            createProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+
+        binding = FragmentLoginBinding.bind(view)
+        binding.apply {
+
+            create.setText(viewModel.email)
+            loginPassword.setText(viewModel.password)
+
+            loginUsername.addTextChangedListener {
+                viewModel.email = it.toString()
             }
+            loginPassword.addTextChangedListener {
+                viewModel.password = it.toString()
+            }
+
+
+            btnLogin.setOnClickListener {
+                viewModel.login()
+            }
+
+            textForgotPassword.setOnClickListener {
+//                findNavController().navigate(loginFragmentDirections.actionLoginFragmentToForgotPasswordFragment())
+            }
+
+
+        }
+
+        viewLifecycleOwner.lifecycleScope.launchWhenStarted {
+
+            viewModel.createProfileEvent.collect { events ->
+                when (events) {
+                    is createProfileViewModel.CreaterProfileEvent.NavigateBackWithResult -> {
+                        Intent(requireContext(), DashboardActivity::class.java).also {
+                            startActivity(it)
+                            requireActivity().finish()
+                        }
+                    }
+                    is createProfileViewModel.CreaterProfileEvent.ShowErrorMessage -> {
+                        Snackbar.make(requireView(), events.msg, Snackbar.LENGTH_LONG).show()
+
+                    }
+                }.exhaustive
+            }
+
+        }
+
     }
+
 }
+
+
+val <T> T.exhaustive: T
+    get() = this
--- app/src/main/java/com/dhairya/societymanagementapplication/authActivity/authfragments/ui/createProfile/createProfileViewModel.kt
+++ app/src/main/java/com/dhairya/societymanagementapplication/authActivity/authfragments/ui/createProfile/createProfileViewModel.kt
@@ -1,4 +1,72 @@
 package com.dhairya.societymanagementapplication.authActivity.authfragments.ui.createProfile
 
-class createProfileViewModel {
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.dhairya.societymanagementapplication.authActivity.AUTH_RESULT_OK
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import java.lang.Exception
+
+class createProfileViewModel constructor(
+    private val state: SavedStateHandle // it's store all data of fragment in a bundle when app goes in onpause state
+) : ViewModel() {
+
+    //Live data events
+
+    private val createProfileEventChannel = Channel<CreaterProfileEvent>()
+    val createProfileEvent = createProfileEventChannel.receiveAsFlow()
+
+
+    private val auth = FirebaseAuth.getInstance()
+
+    //fetch data from fregement
+    var email = state.get<String>("email") ?: ""
+        set(value) {
+            field = value
+            state.set("email", value)
+        }
+
+    var password = state.get<String>("password") ?: ""
+        set(value) {
+            field = value
+            state.set("password", value)
+        }
+
+    //fun for login
+    fun login() {
+        if (password.isBlank() || email.isBlank()) {
+            val error = "The field must not be empty"
+            showErrorMessage(error)
+            return
+        } else {
+            viewModelScope.launch(Dispatchers.IO) {
+                try {
+                    auth.signInWithEmailAndPassword(email, password).await()
+                    createProfileEventChannel.send(CreaterProfileEvent.NavigateBackWithResult(AUTH_RESULT_OK))
+                } catch (e: Exception) {
+                    showErrorMessage(e.message.toString())
+                }
+            }
+        }
+
+
+    }
+
+
+
+
+    private fun showErrorMessage(text: String) = viewModelScope.launch {
+        createProfileEventChannel.send(CreaterProfileEvent.ShowErrorMessage(text))
+    }
+
+    sealed class CreaterProfileEvent {
+        data class ShowErrorMessage(val msg: String) : CreaterProfileEvent()
+        data class NavigateBackWithResult(val result: Int) : CreaterProfileEvent()
+    }
+
 }
