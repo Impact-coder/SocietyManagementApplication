ae42293c74a4a4a4c087489eec3a233d762d8c0b
Hardik <hardikvaghasiya69@gmail.com>
2022-03-14 23:52:27 +0530

    commit 7.10

--- app/src/main/java/com/dhairya/societymanagementapplication/authActivity/authfragments/ui/createProfile/createProfileFragment.kt
+++ app/src/main/java/com/dhairya/societymanagementapplication/authActivity/authfragments/ui/createProfile/createProfileFragment.kt
@@ -1,51 +1,141 @@
 package com.dhairya.societymanagementapplication.authActivity.authfragments.ui.createProfile
 
+import android.app.Activity
+import android.app.Activity.RESULT_OK
+import android.content.Context
 import android.content.Intent
+import android.content.RestrictionsManager.RESULT_ERROR
+import android.icu.number.NumberFormatter.with
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import androidx.navigation.fragment.findNavController
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.Nullable
+import androidx.core.app.ActivityCompat
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import com.dhairya.societymanagementapplication.R
 import com.dhairya.societymanagementapplication.dashboardActivity.DashboardActivity
+import com.dhairya.societymanagementapplication.databinding.FragmentCreateProfileBinding
 import com.dhairya.societymanagementapplication.databinding.FragmentLoginBinding
+import com.google.android.gms.cast.framework.media.ImagePicker
 import com.google.android.material.snackbar.Snackbar
+import com.theartofdev.edmodo.cropper.CropImage
+import com.theartofdev.edmodo.cropper.CropImageActivity
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.os.Build
+import android.widget.ImageView
+import androidx.annotation.RequiresApi
+
+
 import kotlinx.coroutines.flow.collect
+import android.provider.MediaStore
+
+import android.R.attr.name
+
+
+
 
 
 class createProfileFragment : Fragment(R.layout.fragment_create_profile) {
 
-    private val viewModel: createProfileViewModel by viewModel()
-    private lateinit var binding: FragmentLoginBinding
+    private lateinit var cropActivityResultLauncher: ActivityResultLauncher<Any?>
 
+
+    private val mPickImage = 1
+    private lateinit var mYourBitmap: Bitmap
+
+
+
+    private val cropActivityResultContract= object : ActivityResultContract<Any?, Uri?>(){
+        override fun createIntent(context: Context, input: Any?): Intent {
+            return CropImage.activity()
+                .setAspectRatio(16,9)
+                .getIntent(context)
+
+        }
+
+        override fun parseResult(resultCode: Int, intent: Intent?): Uri? {
+            return CropImage.getActivityResult(intent)?.uri
+        }
+
+    }
+
+    private val viewModel: createProfileViewModel by viewModels()
+    private lateinit var binding: FragmentCreateProfileBinding
+
+    @RequiresApi(Build.VERSION_CODES.P)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
 
-        binding = FragmentLoginBinding.bind(view)
+
+
+
+
+
+        binding = FragmentCreateProfileBinding.bind(view)
         binding.apply {
 
-            create.setText(viewModel.email)
-            loginPassword.setText(viewModel.password)
-
-            loginUsername.addTextChangedListener {
-                viewModel.email = it.toString()
-            }
-            loginPassword.addTextChangedListener {
-                viewModel.password = it.toString()
-            }
+            cropActivityResultLauncher = registerForActivityResult(cropActivityResultContract){
+                it?.let { uri ->
 
 
-            btnLogin.setOnClickListener {
-                viewModel.login()
+
+                    val source = ImageDecoder.createSource(context?.contentResolver!!, uri)
+                    mYourBitmap = ImageDecoder.decodeBitmap(source)
+                    val resized = Bitmap.createScaledBitmap(mYourBitmap, 300, 300, true)
+
+                    btnChoosePic.setImageBitmap(resized)
+
+                }
             }
 
-            textForgotPassword.setOnClickListener {
-//                findNavController().navigate(loginFragmentDirections.actionLoginFragmentToForgotPasswordFragment())
+            createProfileName.setText(viewModel.createprofilename)
+            createProfileMobileNo.setText(viewModel.createprofilemobileno)
+            createProfileEmail.setText(viewModel.createprofileemail)
+            createProfileFlatNo.setText(viewModel.createprofileflatno)
+
+
+            createProfileName.addTextChangedListener {
+                viewModel.createprofilename = it.toString()
             }
+            createProfileMobileNo.addTextChangedListener {
+                viewModel.createprofilemobileno = it.toString()
+            }
+
+            createProfileEmail.addTextChangedListener {
+                viewModel.createprofileemail = it.toString()
+            }
+
+            createProfileFlatNo.addTextChangedListener {
+                viewModel.createprofileflatno = it.toString()
+            }
+
+
+            btnCreateProfile.setOnClickListener {
+                viewModel.createProfile()
+            }
+
+            btnChoosePic.setOnClickListener {
+
+
+
+                cropActivityResultLauncher.launch(null)
+
+
+
+            }
+
 
 
         }
@@ -70,6 +160,13 @@ class createProfileFragment : Fragment(R.layout.fragment_create_profile) {
         }
 
     }
+
+
+
+
+
+
+
 
 }
 
--- app/src/main/java/com/dhairya/societymanagementapplication/authActivity/authfragments/ui/createProfile/createProfileViewModel.kt
+++ app/src/main/java/com/dhairya/societymanagementapplication/authActivity/authfragments/ui/createProfile/createProfileViewModel.kt
@@ -1,5 +1,11 @@
 package com.dhairya.societymanagementapplication.authActivity.authfragments.ui.createProfile
 
+import android.app.Activity
+import android.app.appsearch.AppSearchResult.RESULT_OK
+import android.content.Intent
+import android.net.Uri
+import androidx.core.app.ActivityCompat
+import androidx.core.app.ActivityCompat.startActivityForResult
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -11,11 +17,19 @@ import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
 import java.lang.Exception
+import androidx.core.app.ActivityCompat.startActivityForResult
+//import sun.font.LayoutPathImpl.getPath
+
+
+
+
+
+
 
 class createProfileViewModel constructor(
     private val state: SavedStateHandle // it's store all data of fragment in a bundle when app goes in onpause state
 ) : ViewModel() {
-
+    private val SELECT_PICTURE = 1
     //Live data events
 
     private val createProfileEventChannel = Channel<CreaterProfileEvent>()
@@ -25,29 +39,43 @@ class createProfileViewModel constructor(
     private val auth = FirebaseAuth.getInstance()
 
     //fetch data from fregement
-    var email = state.get<String>("email") ?: ""
+    var createprofilename = state.get<String>("createprofilename") ?: ""
         set(value) {
             field = value
-            state.set("email", value)
+            state.set("createprofilename", value)
         }
 
-    var password = state.get<String>("password") ?: ""
+    var createprofileemail = state.get<String>("createprofileemail") ?: ""
         set(value) {
             field = value
-            state.set("password", value)
+            state.set("createprofileemail", value)
         }
+
+    var createprofilemobileno = state.get<String>("createprofilemobileno") ?: ""
+        set(value) {
+            field = value
+            state.set("createprofilemobileno", value)
+        }
+
+    var createprofileflatno = state.get<String>("createprofileflatno") ?: ""
+        set(value) {
+            field = value
+            state.set("createprofileflatno", value)
+        }
+
+
 
     //fun for login
-    fun login() {
-        if (password.isBlank() || email.isBlank()) {
-            val error = "The field must not be empty"
+    fun createProfile() {
+        if (createprofilename.isBlank() || createprofileemail.isBlank() || createprofilemobileno.isBlank() || createprofileflatno.isBlank()) {
+            val error = "The field must not be empty!!"
             showErrorMessage(error)
             return
         } else {
             viewModelScope.launch(Dispatchers.IO) {
                 try {
-                    auth.signInWithEmailAndPassword(email, password).await()
-                    createProfileEventChannel.send(CreaterProfileEvent.NavigateBackWithResult(AUTH_RESULT_OK))
+//                    auth.signInWithEmailAndPassword(email, password).await()
+//                    createProfileEventChannel.send(CreaterProfileEvent.NavigateBackWithResult(AUTH_RESULT_OK))
                 } catch (e: Exception) {
                     showErrorMessage(e.message.toString())
                 }
@@ -62,6 +90,23 @@ class createProfileViewModel constructor(
 
     private fun showErrorMessage(text: String) = viewModelScope.launch {
         createProfileEventChannel.send(CreaterProfileEvent.ShowErrorMessage(text))
+    }
+
+//    private fun openGalleryForImage() {
+//
+//    }
+//
+//    fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+//        if (requestCode ==2 && resultCode == RESULT_OK && data != null){
+//
+//            imageUri = data.getData();
+////            imageView.setImageURI(imageUri);
+//
+//        }
+//    }
+
+    fun choosePick() {
+
     }
 
     sealed class CreaterProfileEvent {
