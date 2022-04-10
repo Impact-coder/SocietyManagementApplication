78f115e256a7b67a1abbf8914af1388ab2e821c0
Hardik <hardikvaghasiya69@gmail.com>
2022-04-10 23:34:49 +0530

    commit 9.5

--- app/src/main/java/com/dhairya/societymanagementapplication/authActivity/authfragments/ui/changePassword/changePasswordFragment.kt
+++ app/src/main/java/com/dhairya/societymanagementapplication/authActivity/authfragments/ui/changePassword/changePasswordFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.viewModels
 import com.dhairya.societymanagementapplication.R
@@ -19,7 +20,7 @@ class changePasswordFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
+        Toast.makeText(context, "this is changePassword!", Toast.LENGTH_SHORT).show()
 
         binding = FragmentChangePasswordBinding.bind(view)
         binding.apply {
--- app/src/main/java/com/dhairya/societymanagementapplication/authActivity/authfragments/ui/login/loginFragment.kt
+++ app/src/main/java/com/dhairya/societymanagementapplication/authActivity/authfragments/ui/login/loginFragment.kt
@@ -12,9 +12,18 @@ import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.dhairya.societymanagementapplication.R
 import com.dhairya.societymanagementapplication.dashboardActivity.DashboardActivity
+import com.dhairya.societymanagementapplication.data.profileData
+import com.dhairya.societymanagementapplication.data.residentsData
 import com.dhairya.societymanagementapplication.databinding.FragmentLoginBinding
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 
 class loginFragment : Fragment(R.layout.fragment_login) {
 
@@ -22,14 +31,28 @@ class loginFragment : Fragment(R.layout.fragment_login) {
     private lateinit var binding: FragmentLoginBinding
     lateinit var sharedPreferences: SharedPreferences
 
+    private val resident = FirebaseFirestore.getInstance().collection("residents")
+    private var resi_Data: MutableList<residentsData> = mutableListOf()
+    var isPasswordReset:String = ""
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        sharedPreferences = this.activity!!.getSharedPreferences("myprefs", Context.MODE_PRIVATE)
-        val editor = sharedPreferences.edit()
+//        sharedPreferences = this.activity!!.getSharedPreferences("myprefs", Context.MODE_PRIVATE)
+//        val editor = sharedPreferences.edit()
+//
+//        editor.putString("isChangePassWord", "no")
+//        editor.putString("isProfileCretaed","no")
 
-        editor.putString("isChangePassWord", "no")
-        editor.putString("isProfileCretaed","no")
+
+
+        CoroutineScope(Dispatchers.Main).launch{
+
+            resi_Data =  resident.whereEqualTo("uid", Firebase.auth.currentUser!!.uid).get()
+                .await().toObjects(residentsData::class.java)
+
+            isPasswordReset = resi_Data[0].password
+        }
 
 
         binding = FragmentLoginBinding.bind(view)
@@ -62,12 +85,21 @@ class loginFragment : Fragment(R.layout.fragment_login) {
                 when (events) {
                     is loginViewModel.LoginEvent.NavigateBackWithResult -> {
 
-
-                        Intent(requireContext(), DashboardActivity::class.java).also {
-                            startActivity(it)
-                            requireActivity().finish()
+                        if (isPasswordReset.isEmpty())
+                        {
+                            findNavController().navigate(loginFragmentDirections.actionLoginFragmentToChangePasswordFragment())
                         }
-                    }
+                        else {
+                            Intent(requireContext(), DashboardActivity::class.java).also {
+                                startActivity(it)
+                                requireActivity().finish()
+                            }
+                        }
+
+
+
+                        }
+
                     is loginViewModel.LoginEvent.ShowErrorMessage -> {
                         Snackbar.make(requireView(), events.msg, Snackbar.LENGTH_LONG).show()
 
