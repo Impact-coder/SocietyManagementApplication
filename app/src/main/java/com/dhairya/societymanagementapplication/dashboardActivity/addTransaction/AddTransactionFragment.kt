1461cc3e6276290ed2ec851a9aca01b0f8fa60d8
sdhairya <sdhairya412@gmail.com>
2022-04-25 23:58:30 +0530

    cp

--- app/src/main/java/com/dhairya/societymanagementapplication/dashboardActivity/addTransaction/AddTransactionFragment.kt
+++ app/src/main/java/com/dhairya/societymanagementapplication/dashboardActivity/addTransaction/AddTransactionFragment.kt
@@ -1,11 +1,17 @@
 package com.dhairya.societymanagementapplication.dashboardActivity.addTransaction
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.DatePicker
 import com.dhairya.societymanagementapplication.R
+import com.dhairya.societymanagementapplication.databinding.FragmentAddTransactionBinding
+import com.dhairya.societymanagementapplication.databinding.FragmentExpenseSheetBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,44 +23,42 @@ private const val ARG_PARAM2 = "param2"
  * Use the [AddTransactionFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class AddTransactionFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class AddTransactionFragment : Fragment(R.layout.fragment_add_transaction) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding : FragmentAddTransactionBinding
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_transaction, container, false)
-    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AddTransactionFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AddTransactionFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        var cal = Calendar.getInstance()
+        var date=""
+
+        binding = FragmentAddTransactionBinding.bind(view)
+        binding.apply {
+
+            val dateSetListener =
+                DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                    cal.set(Calendar.YEAR, year)
+                    cal.set(Calendar.MONTH, monthOfYear)
+                    cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                    val myFormat = "dd/MM/yyyy" // mention the format you need
+                    val sdf = SimpleDateFormat(myFormat, Locale.US)
+                    btnSelectDate.textSize= 20F
+                    date=sdf.format(cal.getTime())
+                    btnSelectDate.text = sdf.format(cal.getTime())
                 }
+
+            btnSelectDate.setOnClickListener {
+                DatePickerDialog(requireContext(),
+                    dateSetListener,
+                    // set DatePickerDialog to point to today's date when it loads up
+                    cal.get(Calendar.YEAR),
+                    cal.get(Calendar.MONTH),
+                    cal.get(Calendar.DAY_OF_MONTH)).show()
             }
+
+        }
+
     }
+
 }
--- app/src/main/java/com/dhairya/societymanagementapplication/dashboardActivity/expenseSheet/expenseSheetFragment.kt
+++ app/src/main/java/com/dhairya/societymanagementapplication/dashboardActivity/expenseSheet/expenseSheetFragment.kt
@@ -27,8 +27,8 @@ class expenseSheetFragment : Fragment(R.layout.fragment_expense_sheet) {
 
     private lateinit var binding : FragmentExpenseSheetBinding
 
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
         super.onViewCreated(view, savedInstanceState)
 
         var cal = Calendar.getInstance()
@@ -36,19 +36,16 @@ class expenseSheetFragment : Fragment(R.layout.fragment_expense_sheet) {
         binding = FragmentExpenseSheetBinding.bind(view)
         binding.apply {
 
-            val dateSetListener = object : DatePickerDialog.OnDateSetListener {
-                override fun onDateSet(view: DatePicker, year: Int, monthOfYear: Int,
-                                       dayOfMonth: Int) {
+            val dateSetListener =
+                DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
                     cal.set(Calendar.YEAR, year)
                     cal.set(Calendar.MONTH, monthOfYear)
                     cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
                     val myFormat = "dd/MM/yyyy" // mention the format you need
                     val sdf = SimpleDateFormat(myFormat, Locale.US)
                     startDate.textSize= 17F
-
                     startDate.text = sdf.format(cal.getTime())
                 }
-            }
 
             startDate.setOnClickListener {
                     DatePickerDialog(requireContext(),
@@ -57,6 +54,14 @@ class expenseSheetFragment : Fragment(R.layout.fragment_expense_sheet) {
                         cal.get(Calendar.YEAR),
                         cal.get(Calendar.MONTH),
                         cal.get(Calendar.DAY_OF_MONTH)).show()
+            }
+            endDate.setOnClickListener {
+                DatePickerDialog(requireContext(),
+                    dateSetListener,
+                    // set DatePickerDialog to point to today's date when it loads up
+                    cal.get(Calendar.YEAR),
+                    cal.get(Calendar.MONTH),
+                    cal.get(Calendar.DAY_OF_MONTH)).show()
             }
 
         }
