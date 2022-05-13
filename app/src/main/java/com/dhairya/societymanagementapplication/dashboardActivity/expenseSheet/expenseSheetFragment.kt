16c253f73a30469ec2b2553caf03ff949c518fd8
sdhairya <sdhairya412@gmail.com>
2022-05-13 23:01:29 +0530

    cp

--- app/src/main/java/com/dhairya/societymanagementapplication/dashboardActivity/expenseSheet/expenseSheetFragment.kt
+++ app/src/main/java/com/dhairya/societymanagementapplication/dashboardActivity/expenseSheet/expenseSheetFragment.kt
@@ -1,10 +1,19 @@
 package com.dhairya.societymanagementapplication.dashboardActivity.expenseSheet
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.app.DatePickerDialog
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.Environment
+import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
-import androidx.core.widget.addTextChangedListener
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -19,6 +28,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
+import java.io.File
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -41,6 +51,12 @@ class expenseSheetFragment : Fragment(R.layout.fragment_expense_sheet) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
         super.onViewCreated(view, savedInstanceState)
+
+        val directory =
+            File(Environment.getExternalStorageDirectory().toString() + "/Aalishan Committee")
+        if (!directory.exists() || !directory.isDirectory()) {
+            directory.mkdirs()
+        }
 
         var cal = Calendar.getInstance()
 
@@ -156,6 +172,239 @@ class expenseSheetFragment : Fragment(R.layout.fragment_expense_sheet) {
                 findNavController().navigate(expenseSheetFragmentDirections.actionExpenseSheetFragmentToDashBoardFragment())
             }
         }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 0) {
+            if (grantResults[0] == PackageManager.PERMISSION_DENIED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        requireActivity(),
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    )
+                ) {
+                    val dialog: AlertDialog.Builder = AlertDialog.Builder(requireActivity())
+                    dialog.setMessage("This permission is required to export data to excel file, if you deny you will not be able to use that feature")
+                        .setTitle("Important permission required")
+                    dialog.setPositiveButton("Okay",
+                        DialogInterface.OnClickListener { dialog, which -> requestPermission() })
+                    dialog.setNegativeButton("NO Thanks!",
+                        DialogInterface.OnClickListener { dialog, which ->
+                            Toast.makeText(
+                                this@MainActivity,
+                                "You will not be able to use some features!",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        })
+                    dialog.show()
+                } else {
+                    Toast.makeText(
+                        this,
+                        "Please accept storage permission to use some features!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+            UNIQUE_CODE
+        )
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.getItemId() === R.id.faqs) {
+            startActivity(Intent(this@MainActivity, FAQs::class.java))
+        } else if (item.getItemId() === R.id.contactDeveloper) {
+            startActivity(Intent(this@MainActivity, ContactDeveloper::class.java))
+        } else if (item.getItemId() === R.id.createExcel) {
+            val filePath = File(
+                Environment.getExternalStorageDirectory()
+                    .toString() + "/Aalishan Committee/Data.xls"
+            )
+            if (ContextCompat.checkSelfPermission(
+                    this@MainActivity,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                requestPermission()
+            } else {
+                val hssfWorkbook = HSSFWorkbook()
+                val hssfSheet: HSSFSheet = hssfWorkbook.createSheet("Data")
+                var hssfRow: HSSFRow = hssfSheet.createRow(0)
+                var hssfCell: HSSFCell = hssfRow.createCell(0)
+                hssfCell.setCellValue("Flat No")
+                hssfCell = hssfRow.createCell(1)
+                hssfCell.setCellValue("Owner Name")
+                hssfCell = hssfRow.createCell(2)
+                hssfCell.setCellValue("Owner Number")
+                hssfCell = hssfRow.createCell(3)
+                hssfCell.setCellValue("Living in house")
+                hssfCell = hssfRow.createCell(4)
+                hssfCell.setCellValue("Maintenance")
+                hssfSheet.setColumnWidth(0, 15 * 150)
+                hssfSheet.setColumnWidth(1, 15 * 200)
+                hssfSheet.setColumnWidth(2, 15 * 220)
+                hssfSheet.setColumnWidth(3, 15 * 230)
+                hssfSheet.setColumnWidth(4, 15 * 210)
+                for (i in 0 until ApplicationClass.flats.size()) {
+                    var j = 0
+                    hssfRow = hssfSheet.createRow(i + 1)
+                    hssfCell = hssfRow.createCell(j)
+                    j++
+                    hssfCell.setCellValue(ApplicationClass.flats.get(i).getFlatNo())
+                    hssfCell = hssfRow.createCell(j)
+                    j++
+                    hssfCell.setCellValue(ApplicationClass.flats.get(i).getOwnerName())
+                    hssfCell = hssfRow.createCell(j)
+                    j++
+                    hssfCell.setCellValue(ApplicationClass.flats.get(i).getOwnerNumber())
+                    hssfCell = hssfRow.createCell(j)
+                    j++
+                    hssfCell.setCellValue(ApplicationClass.flats.get(i).getLivingInHouse())
+                    hssfCell = hssfRow.createCell(j)
+                    hssfCell.setCellValue(
+                        ApplicationClass.flats.get(i).getLastMaintenanceMonth().toString() + " " +
+                                ApplicationClass.flats.get(i).getLastMaintenanceYear()
+                    )
+                }
+                try {
+                    if (!filePath.exists()) {
+                        filePath.createNewFile()
+                    }
+                    val fileOutputStream: FileOutputStream = FileOutputStream(filePath)
+                    hssfWorkbook.write(fileOutputStream)
+                    Toast.makeText(
+                        this,
+                        "File created at Storage/Aalishan Committee/Data.xml",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    if (fileOutputStream != null) {
+                        fileOutputStream.flush()
+                        fileOutputStream.close()
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        } else if (item.getItemId() === R.id.createExcelFilter) {
+            val filePath = File(
+                Environment.getExternalStorageDirectory()
+                    .toString() + "/Aalishan Committee/Data with filter.xls"
+            )
+            val c = Calendar.getInstance()
+            val year = c[Calendar.YEAR]
+            val month = c[Calendar.MONTH]
+            if (ContextCompat.checkSelfPermission(
+                    this@MainActivity,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                requestPermission()
+            } else {
+                val hssfWorkbook = HSSFWorkbook()
+                val hssfSheet: HSSFSheet = hssfWorkbook.createSheet("Data with filter")
+                var k = 0
+                for (i in 0 until ApplicationClass.flats.size()) {
+                    val year1: Int = ApplicationClass.flats.get(i).getLastMaintenanceYear().toInt()
+                    val month1: String =
+                        ApplicationClass.flats.get(i).getLastMaintenanceMonth().trim()
+                    val mon: Int = getMonthIndex(month1)
+                    if (year1 <= year && mon <= month) {
+                        var hssfRow: HSSFRow = hssfSheet.createRow(0)
+                        var hssfCell: HSSFCell = hssfRow.createCell(0)
+                        hssfCell.setCellValue("Flat No")
+                        hssfCell = hssfRow.createCell(1)
+                        hssfCell.setCellValue("Owner Name")
+                        hssfCell = hssfRow.createCell(2)
+                        hssfCell.setCellValue("Owner Number")
+                        hssfCell = hssfRow.createCell(3)
+                        hssfCell.setCellValue("Living in house")
+                        hssfCell = hssfRow.createCell(4)
+                        hssfCell.setCellValue("Maintenance")
+                        hssfSheet.setColumnWidth(0, (15 * 150))
+                        hssfSheet.setColumnWidth(1, (15 * 200))
+                        hssfSheet.setColumnWidth(2, (15 * 220))
+                        hssfSheet.setColumnWidth(3, (15 * 230))
+                        hssfSheet.setColumnWidth(4, (15 * 210))
+                        var j = 0
+                        hssfRow = hssfSheet.createRow(k + 1)
+                        hssfCell = hssfRow.createCell(j)
+                        j++
+                        hssfCell.setCellValue(ApplicationClass.flats.get(i).getFlatNo())
+                        hssfCell = hssfRow.createCell(j)
+                        j++
+                        hssfCell.setCellValue(ApplicationClass.flats.get(i).getOwnerName())
+                        hssfCell = hssfRow.createCell(j)
+                        j++
+                        hssfCell.setCellValue(ApplicationClass.flats.get(i).getOwnerNumber())
+                        hssfCell = hssfRow.createCell(j)
+                        j++
+                        hssfCell.setCellValue(ApplicationClass.flats.get(i).getLivingInHouse())
+                        hssfCell = hssfRow.createCell(j)
+                        hssfCell.setCellValue(
+                            (ApplicationClass.flats.get(i).getLastMaintenanceMonth()
+                                .toString() + " " +
+                                    ApplicationClass.flats.get(i).getLastMaintenanceYear())
+                        )
+                        k++
+                        try {
+                            if (!filePath.exists()) {
+                                filePath.createNewFile()
+                            }
+                            val fileOutputStream: FileOutputStream = FileOutputStream(filePath)
+                            hssfWorkbook.write(fileOutputStream)
+                            Toast.makeText(
+                                this,
+                                "File created at Storage/Aalishan Committee/Data with filter.xml",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            if (fileOutputStream != null) {
+                                fileOutputStream.flush()
+                                fileOutputStream.close()
+                            }
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                        }
+                    }
+                }
+            }
+        } else if (item.getItemId() === R.id.societyRules) {
+            startActivity(Intent(this@MainActivity, SocietyRules::class.java))
+        } else if (item.getItemId() === R.id.logout) {
+            showProgress(true)
+            tvLoad.setText("Logging you out... Please wait...")
+            Backendless.UserService.logout(object : AsyncCallback<Void?>() {
+                fun handleResponse(response: Void?) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Logged out successfully!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    startActivity(Intent(this@MainActivity, Login::class.java))
+                    this@MainActivity.finish()
+                }
+
+                fun handleFault(fault: BackendlessFault) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Error: " + fault.getDetail(),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    showProgress(false)
+                }
+            })
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 
