96bb287b7b55612d70cde3f01bdd214b09e92998
Hardik <hardikvaghasiya69@gmail.com>
2022-05-13 12:43:53 +0530

    commit 11.2

--- app/src/main/java/com/dhairya/societymanagementapplication/data/complainData.kt
+++ app/src/main/java/com/dhairya/societymanagementapplication/data/complainData.kt
@@ -1,13 +1,13 @@
 package com.dhairya.societymanagementapplication.data
 
 data class complainData(
-    val complainDate: String,
-    val flatNO:String,
-    val memberName:String,
-    val complainSubject: String,
-    val complain: String,
-    val memberId: String,
-    val complainResponse: String,
-    val cid: String
+    val complainDate: String= "",
+    val flatNO:String="",
+    val memberName:String="",
+    val complainSubject: String="",
+    val complain: String="",
+    val memberId: String="",
+    val complainResponse: String="",
+    val cid: String=""
 
 )
