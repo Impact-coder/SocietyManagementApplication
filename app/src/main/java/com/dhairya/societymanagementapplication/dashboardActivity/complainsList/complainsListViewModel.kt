9994022088c0c6314c863d8cbaad820df6f6e66d
sdhairya <sdhairya412@gmail.com>
2022-05-15 15:24:14 +0530

    cp

--- app/src/main/java/com/dhairya/societymanagementapplication/dashboardActivity/complainsList/complainsListViewModel.kt
+++ app/src/main/java/com/dhairya/societymanagementapplication/dashboardActivity/complainsList/complainsListViewModel.kt
@@ -3,7 +3,7 @@ package com.dhairya.societymanagementapplication.dashboardActivity.complainsList
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 
-class complainsListViewModel(
+class  complainsListViewModel(
     private val state: SavedStateHandle
 ) : ViewModel() {
 
